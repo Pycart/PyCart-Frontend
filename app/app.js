be969115fedb7c8739a02fe554fd97a1a5cdd63d
John Borell <assaltshaker@yahoo.com>
2015-06-04 10:41:17 -0600

    configured app.js controller: includes future user, auth capabilities

--- app/app.js
+++ app/app.js
@@ -5,8 +5,45 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  'myApp.version',
+  'myApp.auth',
+  'myApp.landing',
+  'restangular'
+])
+    .config(['$routeProvider', 'RestangularProvider', function ($routeProvider, RestangularProvider) {
+        $routeProvider.otherwise({redirectTo: '/landing'});
+
+        RestangularProvider.setBaseUrl('http://localhost:8001'); // or the purpose o user setup, this will run on localhost of their machine
+    }])
+
+    .controller('AppCtrl', function ($scope, user, $location, Restangular) {
+        if (sessionStorage.getItem('DjangoAuthToken')) {
+            var token = sessionStorage.getItem('DjangoAuthToken');
+            Restangular.setDefaultHeaders({Authorization: 'Token ' + token});
+            user.getInfo().then(function () {
+                $scope.user = user.info;
+                $location.path('/landing');
+            });
+        }
+
+        if (user.info.id == '') {
+            $location.path('/landing');
+        }
+
+        $scope.logout = function () {
+            user.logout();
+            $scope.user = null;
+            $location.path('/landing');
+        };
+
+        $scope.$on("user-updated", function () {
+            $scope.user = user.info;
+        });
+        //Locks down all routes. This would require you to log in.
+        $scope.$on('$routeChangeStart', function () {
+            if ((user.info != null && user.info.id == '') || user.info == null) {
+                $location.path('/landing');
+            }
+        });
+
+    });
