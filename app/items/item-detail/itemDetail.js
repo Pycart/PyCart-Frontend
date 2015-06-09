ea9ffed021968480c24b591d261c29f01d0e18f3
Tim <chent1101@gmail.com>
2015-06-09 16:02:44 -0600

    Added item detail modal to display item details

--- app/items/item-detail/itemDetail.js
+++ app/items/item-detail/itemDetail.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.itemDetail', ['ngRoute'])
+angular.module('myApp.itemDetail', ['ngRoute', 'ui.bootstrap'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/itemDetail/:itemId', {
@@ -9,9 +9,31 @@ angular.module('myApp.itemDetail', ['ngRoute'])
         });
     }])
 
+    .service('ItemDetailModal', ['$modal', function ($modal) {
+        return {
+            open: function (item) {
+                $modal.open({
+                    templateUrl: 'items/item-detail/item-detail.html',
+                    controller: 'ItemDetailModalCtrl',
+                    resolve: {
+                        itm: function () {
+                            return item;
+                        }
+                    }
+                });
+            }
+        };
+    }])
+
     .controller('ItemDetailCtrl', ['$scope','$routeParams', 'Restangular', function ($scope, $routeParams, Restangular) {
         $scope.itemId = $routeParams.itemId;
         Restangular.one('items_detail', $scope.itemId).customGET().then(function (item) {
             $scope.item = item;
         });
+
+    }])
+
+    .controller('ItemDetailModalCtrl', ['$scope', 'itm', function ($scope, itm) {
+        $scope.item = itm
+
     }]);
