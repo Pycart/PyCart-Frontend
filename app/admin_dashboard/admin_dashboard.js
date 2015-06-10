c87b8c79821f330e0d0e12e51c809689ab749d03
John Borell <assaltshaker@yahoo.com>
2015-06-10 12:20:03 -0600

    admin add new item,required new functions for adding options and tags

--- app/admin_dashboard/admin_dashboard.js
+++ app/admin_dashboard/admin_dashboard.js
@@ -10,16 +10,67 @@ angular.module('myApp.adminDashboard', ['ngRoute'])
     }])
 
     .controller('AdminDashboardCtrl', ['$scope', 'Restangular', function ($scope, Restangular) {
-        Restangular.all('admin_dashboard/items').customGET().then(function (data) {
-            $scope.ItemList = data;
-        });
-        Restangular.all('admin_dashboard/options').customGET().then(function (data) {
-            $scope.OptionList = data;
-        });
-        Restangular.all('admin_dashboard/status').customGET().then(function (data) {
-            $scope.StatusList = data;
-        });
+        $scope.newItem = {
+            options: [],
+            tags: []
+        };
+        $scope.GetItemList = function () {
+            Restangular.all('admin_dashboard/items').customGET().then(function (data) {
+                $scope.ItemList = data;
+            });
+        };
+        $scope.GetOptionList = function () {
+            Restangular.all('admin_dashboard/options').customGET().then(function (data) {
+                $scope.OptionList = data;
+            });
+        };
+        $scope.GetStatusList = function () {
+            Restangular.all('admin_dashboard/status').customGET().then(function (data) {
+                $scope.StatusList = data;
+            });
+        };
+        $scope.GetItemList();
+        $scope.GetOptionList();
+        $scope.GetStatusList();
+        $scope.addItemForm = function () {
+            $scope.showItemForm = true;
+        };
+
+        $scope.addTagToItem = function (tagName) {
+                if (tagName != null) {
+                    var tag = {name: tagName};
+                    $scope.newItem.tags.push(tag);
+                    $scope.tagName = null;
+                }
+        };
+
+        $scope.addOptionToItem = function (optionName) {
+                if (optionName != null) {
+                    var option = {name: optionName};
+                    $scope.newItem.options.push(option);
+                    $scope.optionName = null;
+                }
+        };
+
+        $scope.addItem = function (item) {
+            //item.tags = [$scope.newItem.tags];
+            Restangular.one('admin_dashboard/items').customPOST(item).then(function () {
+                $scope.GetItemList();
+                $scope.showItemForm = false;
+                $scope.newItem = null;
+            }, function (error) {
+                alert("There was an error saving your item"); //TODO add a decent error alert
+            })
+        };
+        $scope.addStatus = function (status) {
+            Restangular.one('admin_dashboard/status').customPOST(status).then(function (data) {
+                $scope.GetStatusList();
+            }, function (error) {
+                alert("Error");//TODO add a decent error alert
+            })
+        };
     }]);
+
 
 
 
