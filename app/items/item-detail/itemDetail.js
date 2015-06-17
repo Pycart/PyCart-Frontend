723a97fa6e930cc902f0a8467b7ed7d9391ee406
Joseph Gallagher <uhhhmmm@gmail.com>
2015-06-16 23:35:20 -0400

    Reworked itemDetail with customPUT to add item to current order. Added submit & quantity fields to modal

--- app/items/item-detail/itemDetail.js
+++ app/items/item-detail/itemDetail.js
@@ -1,20 +1,36 @@
 'use strict';
 
-angular.module('myApp.itemDetail', ['ngRoute'])
+angular.module('myApp.itemDetail', ['ngRoute', 'ui.bootstrap'])
 
     .service('ItemDetailModal', ['$modal', function ($modal) {
         return {
             open: function (item) {
                 $modal.open({
                     templateUrl: 'items/item-detail/item-detail.html',
-                    controller: function ($scope) {
-                        $scope.item = item;
-                        $scope.select = function () {
-                            $scope.addToCart += $scope.item;
-                            $modal.close($scope.addToCart);
-                        };
+                    controller: 'ItemDetailModalCtrl',
+                    resolve: {
+                        itm: function () {
+                            return item;
+                        }
                     }
                 });
             }
         };
+    }])
+
+    .controller('ItemDetailModalCtrl', ['$scope', 'itm', 'Restangular', function ($scope, itm, Restangular) {
+        $scope.item = itm;
+
+        //A Quantity of 0 here will effectively delete the item from your current order.
+        //Also, if the user is not logged in, they won't be able to add items to an order.
+        $scope.addItem = function (item, quantity) {
+            Restangular.one('add_to_cart/').customPUT({"items":[item.id], "quantity":[quantity]}).then(function () {
+                alert("Item added to cart!");
+            }, function (error) {
+                alert("There was an error adding your item"); //TODO add a decent error alert here too
+            })
+        };
+
+        //TODO add cancel, update and place current order functionality (need to be able to view the current cart)
+
     }]);
