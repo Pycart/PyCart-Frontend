d8ba79e9bcb3aae589d8781615a5b84dcd912e45
Nate McNeil <nhmcneil@gmail.com>
2015-06-18 17:08:46 -0600

    added item model; replaced Restangular with ;

--- app/items/item-detail/itemDetail.js
+++ app/items/item-detail/itemDetail.js
@@ -1,40 +1,30 @@
 'use strict';
 
-angular.module('myApp.itemDetail', ['ngRoute', 'ui.bootstrap'])
+angular.module('myApp.item')
 
     .service('ItemDetailModal', ['$modal', function ($modal) {
         return {
             open: function (item) {
                 $modal.open({
                     templateUrl: 'items/item-detail/item-detail.html',
-                    controller: 'ItemDetailModalCtrl',
-                    resolve: {
-                        item: function () {
-                            return item;
-                        }
+                    controller: function ($scope, itm, Item) {
+
+	                    $scope.item = itm;
+
+	                    //A Quantity of 0 here will effectively delete the item from your current order.
+	                    //Also, if the user is not logged in, they won't be able to add items to an order.
+	                    $scope.addItem = function (item, quantity) {
+		                    Item.add_to_cart(item, quantity).then(function () {
+			                    alert("Item added to cart!");
+		                    }, function (error) {
+			                    alert("There was an error adding your item"); //TODO add a decent error alert here too
+		                    })
+	                    };
+
+	                    //TODO add cancel, update and place current order functionality (need to be able to view the current cart)
+
                     }
                 });
             }
         };
-    }])
-
-    .controller('ItemDetailModalCtrl', ['$scope', 'item', 'Restangular', function ($scope, item, Restangular) {
-
-        console.log(item);
-        Restangular.one('items/', item).customGET().then(function (data) {
-            $scope.item = data;
-        });
-
-        //A Quantity of 0 here will effectively delete the item from your current order.
-        //Also, if the user is not logged in, they won't be able to add items to an order.
-        $scope.addItem = function (item, quantity) {
-            Restangular.one('cart/add/').customPUT({"items":[item.id], "quantity":[quantity]}).then(function () {
-                alert("Item added to cart!");
-            }, function (error) {
-                alert("There was an error adding your item"); //TODO add a decent error alert here too
-            })
-        };
-
-        //TODO add cancel, update and place current order functionality (need to be able to view the current cart)
-
     }]);
