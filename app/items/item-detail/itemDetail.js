6c347f5d21875e6b7380092b8570dbd531c78917
Coding Campus <cc@codingcamp.us>
2015-06-25 15:52:22 -0600

    conflicts

--- app/items/item-detail/itemDetail.js
+++ app/items/item-detail/itemDetail.js
@@ -7,22 +7,10 @@ angular.module('myApp.item')
             open: function (item) {
                 $modal.open({
                     templateUrl: 'items/item-detail/item-detail.html',
-                    controller: 'ItemDetailModalCtrl',
-                    resolve: {
-                        item: function () {
-                            return item;
-                        }
-                    }
-                });
-            }
-        };
-    }])
+                    controller: function ($scope, Item) {
 
 	                    $scope.item = item;
 
-        Restangular.one('items/', item).customGET().then(function (data) {
-            $scope.item = data;
-        });
 	                    //A Quantity of 0 here will effectively delete the item from your current order.
 	                    //Also, if the user is not logged in, they won't be able to add items to an order.
 	                    $scope.addItem = function (item, quantity) {
@@ -33,16 +21,10 @@ angular.module('myApp.item')
 		                    })
 	                    };
 
-        //A Quantity of 0 here will effectively delete the item from your current order.
-        //Also, if the user is not logged in, they won't be able to add items to an order.
-        $scope.addItem = function (item, quantity) {
-            Restangular.one('cart/add/').customPUT({"items":[item.id], "quantity":[quantity]}).then(function () {
-                alert("Item added to cart!");
-            }, function (error) {
-                alert("There was an error adding your item"); //TODO add a decent error alert here too
-            })
+	                    //TODO add cancel, update and place current order functionality (need to be able to view the current cart)
+
+                    }
+                });
+            }
         };
-
-        //TODO add cancel, update and place current order functionality (need to be able to view the current cart)
-
     }]);
